238f35fe983aa8f9b42b370819d39276af5194ec
shu <chukichi915@gmail.com>
2024-05-21 17:48:27 +0900

    色々いじってずれたので整形する

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,13 +98,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.mailgun.org',
-    port:                 587,
-    domain:               'shu-web-creaton.com',
-    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
-    password:             ENV['MAILGUN_SMTP_PASSWORD'],
-    authentication:       'plain',
-   enable_starttls_auto: true
+    address: 'smtp.mailgun.org',
+    port: 587,
+    domain: 'shu-web-creaton.com',
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
-
 end
