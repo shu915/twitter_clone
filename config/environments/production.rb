fae9a2e1fe86b3a1a854ae104002a8da19c6597a
shu <chukichi915@gmail.com>
2024-05-21 17:15:51 +0900

    本番環境でメールの設定をする

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mailgun.org',
+    port:                 587,
+    domain:               'shu-web-creaton.com',
+    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+    password:             ENV['MAILGUN_SMTP_PASSWORD'],
+    authentication:       'plain',
+   enable_starttls_auto: true
+  }
+
 end
