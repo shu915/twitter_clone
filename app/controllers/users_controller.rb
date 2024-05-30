6295494fbd5a661095c1a1a47eb7d36f53e27b57
shu <chukichi915@gmail.com>
2024-05-30 15:32:20 +0900

    プロフィール編集画面を作った

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,10 @@
 
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: %i[show edit update]
+  before_action :authorize_user, only: %i[edit update]
 
   def show
-    @user = User.find(params[:id])
-
     @tweets = case params[:tab]
               when 'like'
                 @user.liked_tweets.includes(:user,
@@ -20,5 +20,31 @@ class UsersController < ApplicationController
               else
                 @user.tweets.order(created_at: :desc).page(params[:page]).per(10)
               end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = 'プロフィールを変更しました。'
+      redirect_to user_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def authorize_user
+    redirect_to root_path, alert: '権限がありません' unless current_user == @user
+  end
+
+  def user_params
+    params.require(:user).permit(:account_name, :display_name, :tel, :location, :url, :self_intro, :avatar,
+                                 :header_image)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
