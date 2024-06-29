30ce4b5215405095e167a75d5d237795ee13e7be
shu <chukichi915@gmail.com>
2024-06-29 13:33:10 +0900

    messagesのmessageをcontentに変えた

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -7,7 +7,7 @@
 #  id         :bigint           not null, primary key
 #  user_id    :bigint           not null
 #  room_id    :bigint           not null
-#  message    :text
+#  content    :text             not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
